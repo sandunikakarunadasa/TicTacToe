0680b2f1e4c21a942300133ecbd4d750dd618f23
Sandunika Karunadasa <127858906+sandunikakarunadasa@users.noreply.github.com>
2024-06-18 10:36:29 +0530

    Add files via upload

---
+++ Tic_tac_app/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Tic_tac_app"
+include(":app")
